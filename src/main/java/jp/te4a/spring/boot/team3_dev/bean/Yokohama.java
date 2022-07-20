d540677202e82e8c19a76288f05b06c6b0814d00
190735 <190735@e0601-15.JC-21.local>
2022-07-20 09:52:47 +0900

    7/20 キタノ

--- src/main/java/jp/te4a/spring/boot/team3_dev/bean/Yokohama.java
+++ src/main/java/jp/te4a/spring/boot/team3_dev/bean/Yokohama.java
@@ -2,19 +2,39 @@ package jp.te4a.spring.boot.team3_dev.bean;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+
 import javax.persistence.*;
+
+import org.springframework.beans.factory.annotation.Value;
 @Entity
 @Table(name = "yokohama_table")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Yokohama {
-  @Id
-  @GeneratedValue
-  private Integer id ;
-  @Column(nullable = false)
-  private String title;
-  private String writer;
-  private String publisher;
-  private Integer price;
+	@Id
+	private String machine_id ;
+	@Column(nullable = false)
+	private String name;
+	@Column(nullable = false)
+	private String model_number;
+	@Column(nullable = false)
+	private String maker;
+	@Column(nullable = false)
+	private String spec;
+	//purchase_date
+	@Value("#{new java.text.SimpleDateFormat(\"${mydate.format}\").parse(\"${mydate.value}\")}")
+	private Date purchase_date;
+	private int useful_date;
+	private String depreciation;
+	private boolean availability;
+	private boolean loanability;
+	@Column(nullable = false)
+	private String location;
+	//expiration_date
+	@Value("#{new java.text.SimpleDateFormat(\"${mydate.format}\").parse(\"${mydate.value}\")}")
+	private Date expiration_date;
+	private Integer price;
 }
