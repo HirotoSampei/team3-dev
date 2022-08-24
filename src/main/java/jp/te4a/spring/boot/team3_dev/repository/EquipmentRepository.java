41f55f168ca9962fe35bde51a6aebd285584e9cd
190762 <190762@e0601-16.JC-21.local>
2022-08-24 11:27:56 +0900

    commit 8/24 114514

--- src/main/java/jp/te4a/spring/boot/team3_dev/repository/EquipmentRepository.java
+++ src/main/java/jp/te4a/spring/boot/team3_dev/repository/EquipmentRepository.java
@@ -11,7 +11,7 @@ import jp.te4a.spring.boot.team3_dev.bean.Equipment;
 
 @Repository
 public interface EquipmentRepository extends JpaRepository<Equipment, Integer>{
-	@Query("SELECT X FROM Tokyo X ORDER BY X.machine_id")
+	@Query("SELECT X FROM Equipment X ORDER BY X.machine_id")
 	  List<Equipment> findAllOrderByMachine_id();
 
 }
